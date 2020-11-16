febf93528fa2f1ebb58ba8e0f70e9b5bc87c3535
Wayne <wayne.wood@colorado.edu>
2020-11-16 14:58:14 -0700

    quartz scheduler and some modal views

---
+++ src/main/java/Scheduler.java
@@ -0,0 +1,138 @@
+import com.slack.api.bolt.AppConfig;
+import com.slack.api.bolt.App;
+import com.slack.api.bolt.jetty.SlackAppServer;
+import com.slack.api.methods.MethodsClient;
+import com.slack.api.model.User;
+import org.quartz.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+import com.slack.api.Slack;
+import com.slack.api.methods.SlackApiException;
+import org.quartz.SchedulerException;
+import static com.slack.api.model.block.Blocks.*;
+import static com.slack.api.model.block.Blocks.actions;
+import static com.slack.api.model.block.composition.BlockCompositions.markdownText;
+import static com.slack.api.model.block.composition.BlockCompositions.plainText;
+import static com.slack.api.model.block.element.BlockElements.*;
+import static org.quartz.JobBuilder.newJob;
+
+public class Scheduler implements Job {
+
+    private static ArrayList<String> selectedDays;
+    private final Logger logger = LoggerFactory.getLogger("slacker-standup");
+    private final MethodsClient client = Slack.getInstance().methods();
+
+    public Scheduler(){
+        selectedDays = new ArrayList<>();
+    }
+
+    public Scheduler(ArrayList<String> selectedDays) {
+        this.selectedDays = selectedDays;
+    }
+
+    @Override
+    public void execute(JobExecutionContext jec) throws JobExecutionException {
+
+        System.out.println("******");
+        try {
+            var user_result = client.usersList(r -> r
+                .token(System.getenv("SLACK_BOT_TOKEN"))
+            );
+
+            // Call the chat.postMessage method using the built-in WebClient
+            var post_result = client.chatPostMessage(r -> r
+                    // The token you used to initialize your app
+                    .token(System.getenv("SLACK_BOT_TOKEN"))
+                    .channel("U019Q5UCUCS")
+                    .blocks(asBlocks(
+                            section(section -> section.text(markdownText(":wave: Press the button to fill out standup!"))),
+                            actions(actions -> actions
+                                    .elements(asElements(
+                                            button(b -> b.actionId("standup-modal").text(plainText(pt -> pt.text("Enter Standup"))))
+                                    ))
+                            )))
+            );
+
+            logger.info("result {}", post_result);
+
+//            for (User user : user_result.getMembers()) {
+//                // Call the chat.postMessage method using the built-in WebClient
+//                var post_result = client.chatPostMessage(r -> r
+//                        // The token you used to initialize your app
+//                        .token(System.getenv("SLACK_BOT_TOKEN"))
+//                        .channel(user.getId())
+//                        .blocks(asBlocks(
+//                                section(section -> section.text(markdownText(":wave: Press the button to schedule!"))),
+//                                actions(actions -> actions
+//                                        .elements(asElements(
+//                                                button(b -> b.actionId("standup-modal").text(plainText(pt -> pt.text("Enter Standup"))))
+//                                        ))
+//                                )))
+//                );
+//                // Print result, which includes information about the message (like TS)
+//                logger.info("result {}", post_result);
+//                // Store the entire user object (you may not need all of the info)
+////                usersStore.put(user.getId(), user);
+////                System.out.println("1: " + user.getId());
+////                System.out.println("2: " + user.getName());
+//            }
+
+        } catch (IOException e) {
+            logger.error("IO Exception", e);
+        } catch (SlackApiException e) {
+            logger.error("SlackApiException", e);
+        }
+
+        logger.info("executing job");
+    }
+
+    public static void schedule() throws SchedulerException {
+        SchedulerFactory schedFact = new org.quartz.impl.StdSchedulerFactory();
+        
+        org.quartz.Scheduler sched = schedFact.getScheduler();
+
+        sched.start();
+
+        // Delete the job with the trigger
+//        sched.interrupt("myJob");
+//        sched.deleteJob(JobKey.jobKey("myJob", "group1"));
+
+        // define the job and tie it to our HelloJob class
+        JobDetail job = newJob(Scheduler.class)
+                .withIdentity("myJob", "group1")
+                .build();
+
+        // Trigger the job to run now
+        Trigger trigger = TriggerBuilder.newTrigger()
+                .withIdentity("myTrigger", "group1")
+                .startNow()
+                .build();
+
+        // Tell quartz to schedule the job using our trigger
+        sched.scheduleJob(job, trigger);
+
+        System.out.println("!!!!!" + selectedDays.get(0));
+
+        // Trigger job to run when specified
+//        for (String day : this.days) {
+//            // define the job and tie it to our HelloJob class
+//            JobDetail job = newJob(Scheduler.class)
+//                    .withIdentity("myJob", "group" + day)
+//                    .build();
+//
+//            // Trigger the job to run at 10am every day specified
+//            Trigger trigger = TriggerBuilder.newTrigger()
+//                    .withIdentity("myTrigger", "group" + day)
+//                    .withSchedule(cronSchedule("0 0 10 ? * " + day))
+//                    .build();
+//
+//
+//            // Tell quartz to schedule the job using our trigger
+//            sched.scheduleJob(job, trigger);
+//        }
+    }
+}
--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -1,178 +1,182 @@
 import com.slack.api.bolt.App;
 import com.slack.api.bolt.AppConfig;
 import com.slack.api.bolt.jetty.SlackAppServer;
-import com.sun.net.httpserver.*;
-
+import com.slack.api.bolt.response.Response;
+import com.slack.api.methods.response.views.ViewsOpenResponse;
+import com.slack.api.model.view.View;
+import com.slack.api.model.view.ViewState;
 import java.io.IOException;
-import java.io.OutputStream;
-import java.lang.reflect.Field;
-import java.net.http.HttpClient;
-import java.net.http.HttpResponse;
-import java.net.http.HttpRequest;
-import java.net.InetSocketAddress;
-import java.net.URI;
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.nio.charset.StandardCharsets;
-import com.slack.api.webhook.WebhookResponse;
 import com.slack.api.Slack;
 import com.slack.api.methods.SlackApiException;
+import org.quartz.SchedulerException;
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
 import static com.slack.api.model.block.Blocks.*;
 import static com.slack.api.model.block.composition.BlockCompositions.*;
 import static com.slack.api.model.block.element.BlockElements.*;
 import static com.slack.api.model.view.Views.*;
-import com.slack.api.model.event.AppHomeOpenedEvent;
 
 public class Server {
+
+    private static final Logger logger = LoggerFactory.getLogger("slacker-standup");
+    private static final App app = new App();
+
+    static View buildScheduleView() {
+        return view(view -> view
+                .callbackId("schedule-standups")
+                .type("modal")
+                .notifyOnClose(true)
+                .title(viewTitle(title -> title.type("plain_text").text("Standup").emoji(true)))
+                .submit(viewSubmit(submit -> submit.type("plain_text").text("Submit").emoji(true)))
+                .close(viewClose(close -> close.type("plain_text").text("Cancel").emoji(true)))
+                .blocks(asBlocks(
+                        input(input -> input
+                                .label(plainText("Select days for standup:"))
+                                .blockId("days-block")
+                                .element(checkboxes(i -> i
+                                        .actionId("select-days")
+                                        .options(Arrays.asList(
+                                                option(plainText("Monday"), "2"), // value represents cron job day of week value
+                                                option(plainText("Tuesday"), "3"),
+                                                option(plainText("Wednesday"), "4"),
+                                                option(plainText("Thursday"), "5"),
+                                                option(plainText("Friday"), "6")
+                                        ))
+
+                                ))
+                        )
+                ))
+        );
+    }
+
+    static View buildStandupView() {
+        return view(view -> view
+                .callbackId("submission-standups")
+                .type("modal")
+                .notifyOnClose(true)
+                .title(viewTitle(title -> title.type("plain_text").text("Standup").emoji(true)))
+                .submit(viewSubmit(submit -> submit.type("plain_text").text("Submit").emoji(true)))
+                .close(viewClose(close -> close.type("plain_text").text("Cancel").emoji(true)))
+                .blocks(asBlocks(
+                        input(input -> input
+                                .blockId("prev-tasks")
+                                .element(plainTextInput(pti -> pti.actionId("agenda-action").multiline(true)))
+                                .label(plainText(pt -> pt.text("What have you been working on?").emoji(true)))
+                        ),
+                        input(input -> input
+                                .blockId("to-do")
+                                .element(plainTextInput(pti -> pti.actionId("agenda-action").multiline(true)))
+                                .label(plainText(pt -> pt.text("What have you been working on?").emoji(true)))
+                        ),
+                        input(input -> input
+                                .blockId("blockers")
+                                .element(plainTextInput(pti -> pti.actionId("agenda-action").multiline(true)))
+                                .label(plainText(pt -> pt.text("Do you have any blockers?").emoji(true)))
+                        )
+                ))
+        );
+    }
+
     public static void main(String[] args) throws Exception {
-        int PORT = Integer.valueOf(System.getenv("PORT"));
-        HttpServer server = HttpServer.create(new InetSocketAddress(PORT), 0);
-        server.createContext("/command", new CommandHandler());
-//        server.createContext("/oauth", new OauthHandler());
+        var config = new AppConfig();
+        config.setSingleTeamBotToken(System.getenv("SLACK_BOT_TOKEN"));
+        config.setSigningSecret(System.getenv("SLACK_SIGNING_SECRET"));
 
-//        var config = new AppConfig();
-//        config.setSingleTeamBotToken(System.getenv("SLACK_BOT_TOKEN"));
-//        config.setSigningSecret(System.getenv("SLACK_SIGNING_SECRET"));
-        System.out.println("Running on port: " + PORT);
+        App app = Server.app;
+        Logger logger = Server.logger;
 
-        server.setExecutor(null); // creates a default executor
-        server.start();
-
-        var app = new App();
-
-        // All the room in the world for your code
-        app.command("/command", (req, ctx) -> {
-            System.out.println("In command");
+        app.command("/schedule", (req, ctx) -> {
             String commandArgText = req.getPayload().getText();
             String channelId = req.getPayload().getChannelId();
             String channelName = req.getPayload().getChannelName();
             String text = "You said " + commandArgText + " at <#" + channelId + "|" + channelName + ">";
-            System.out.println("returning: " + text);
+
+            var client = Slack.getInstance().methods();
+
+            try {
+                // Call the chat.postMessage method using the built-in WebClient
+                var result = client.chatPostMessage(r -> r
+                                // The token you used to initialize your app
+                                .token(System.getenv("SLACK_BOT_TOKEN"))
+                                .channel(channelId)
+                                .blocks(asBlocks(
+                                    section(section -> section.text(markdownText(":wave: Press the button to schedule!"))),
+                                    actions(actions -> actions
+                                            .elements(asElements(
+                                                    button(b -> b.actionId("schedule-modal").text(plainText(pt -> pt.text("Schedule Standups"))))
+                                            ))
+                                    )))
+                );
+                // Print result, which includes information about the message (like TS)
+                logger.info("result {}", result);
+            } catch (IOException | SlackApiException e) {
+                logger.error("error: {}", e.getMessage(), e);
+            }
+
             return ctx.ack(text); // respond with 200 OK
         });
 
-//        var port = Integer.valueOf(System.getenv("PORT"));
-        var slack_server = new SlackAppServer(app);
+        app.blockAction("schedule-modal", (req, ctx) -> {
+            ViewsOpenResponse viewsOpenRes = ctx.client().viewsOpen(r -> r
+                    .triggerId(ctx.getTriggerId())
+                    .view(buildScheduleView()));
+            if (viewsOpenRes.isOk()) return ctx.ack();
+            else return Response.builder().statusCode(500).body(viewsOpenRes.getError()).build();
+        });
+
+        app.blockAction("standup-modal", (req, ctx) -> {
+            ViewsOpenResponse viewsOpenRes = ctx.client().viewsOpen(r -> r
+                    .triggerId(ctx.getTriggerId())
+                    .view(buildStandupView()));
+            if (viewsOpenRes.isOk()) return ctx.ack();
+            else return Response.builder().statusCode(500).body(viewsOpenRes.getError()).build();
+        });
+
+        app.viewSubmission("schedule-standups", (req, ctx) -> {
+            Map<String, Map<String, ViewState.Value>> stateValues = req.getPayload().getView().getState().getValues();
+            List<ViewState.SelectedOption> days = stateValues.get("days-block").get("select-days").getSelectedOptions();
+
+            ArrayList<String> selectedDays = new ArrayList<>();
+            for (ViewState.SelectedOption element : days) {
+                selectedDays.add(element.getValue());
+            }
+
+            try {
+                Scheduler scheduler = new Scheduler(selectedDays);
+                scheduler.schedule();
+            } catch (SchedulerException e) {
+                e.printStackTrace();
+            }
+            return ctx.ack();
+        });
+
+        app.viewSubmission("submission-standups", (req, ctx) -> {
+            Map<String, Map<String, ViewState.Value>> stateValues = req.getPayload().getView().getState().getValues();
+            List<ViewState.SelectedOption> days = stateValues.get("days-block").get("select-days").getSelectedOptions();
+            String prev_tasks = stateValues.get("prev-tasks");
+
+
+            ArrayList<String> selectedDays = new ArrayList<>();
+            for (ViewState.SelectedOption element : days) {
+                selectedDays.add(element.getValue());
+            }
+
+            try {
+                Scheduler scheduler = new Scheduler(selectedDays);
+                scheduler.schedule();
+            } catch (SchedulerException e) {
+                e.printStackTrace();
+            }
+            return ctx.ack();
+        });
+
+        var port = Integer.valueOf(System.getenv("PORT"));
+        logger.info("RUNNING NOW ON : " + port);
+        var slack_server = new SlackAppServer(app, port);
         slack_server.start();
     }
-
-    static class CommandHandler implements HttpHandler {
-        @Override
-        public void handle(HttpExchange t) throws IOException {
-            String response = "";
-            InputStreamReader isr =  new InputStreamReader(t.getRequestBody(),"utf-8");
-            BufferedReader br = new BufferedReader(isr);
-            String value = br.readLine();
-            isr.close();
-
-            if (value.contains("challenge")) {
-                System.out.println("HELLO");
-                String[] arr = value.split("\"");
-                response = arr[7];
-                System.out.println(response + "!");
-            } else {
-                response = "not found";
-            }
-            t.sendResponseHeaders(200, response.length());
-            OutputStream os = t.getResponseBody();
-            os.write(response.getBytes());
-            os.close();
-            t.close();
-        }
-    }
-
-//    static class CommandHandler implements HttpHandler {
-//        @Override
-//        public void handle(HttpExchange t) throws IOException {
-//            var client = Slack.getInstance().methods();
-//            var logger = LoggerFactory.getLogger("slacker-standup");
-//            try {
-//                // Call the chat.postMessage method using the built-in WebClient
-//                var result = client.chatPostMessage(r -> r
-//                                .token(System.getenv("SLACK_BOT_TOKEN"))
-//                                .channel("D01DU5G7Z7H")
-//                                .text("Testing text")
-//                        // TODO: create blocks[] array to send richer content
-//                );
-//
-//                var otherResult = client.usersList(r -> r
-//                                .token(System.getenv("SLACK_BOT_TOKEN"))
-//                );
-//
-////                var message = client.chatPostMessage(r -> r
-////                                .text("Time for your standup!")
-////                                .attachments(actions())
-////                );
-//
-//                // Send response to request
-//                String response = "Much success!";
-//                t.sendResponseHeaders(200, response.length());
-//                OutputStream os = t.getResponseBody();
-//                os.write(response.getBytes());
-//                os.close();
-//
-//                // Print result in console, which includes information about the message (like TS)
-//                logger.info("result {}", result);
-//            } catch (IOException | SlackApiException e) {
-//                logger.error("error: {}", e.getMessage(), e);
-//            }
-//        }
-//    }
-
-    // If we need to authorize a user, here is some beginning code...
-//    static class OauthHandler implements HttpHandler {
-//        protected static String createUrl(int query_code) {
-//            String query_string = "https://slack.com/api/oauth.access?";
-//            query_string += "code=" + query_code + "&client_id=" + System.getenv("CLIENT_ID") + "&client_secret=" + System.getenv("CLIENT_SECRET");
-//            return query_string;
-//        }
-//
-//        @Override
-//        public void handle(HttpExchange t) throws IOException {
-////            System.out.println(t.getResponseCode());
-//
-//            // parse request body to get code
-////            try (InputStream fis = t.getRequestBody();
-////                 InputStreamReader isr = new InputStreamReader(fis,
-////                         StandardCharsets.UTF_8);
-////                 BufferedReader br = new BufferedReader(isr)) {
-////
-////                br.lines().forEach(line -> System.out.println("*" + line + "*"));
-////            }
-////
-////            if (t.getResponseCode() == -1) {
-////                String response = "Error, no code received.";
-////                t.sendResponseHeaders(500, response.length());
-////                OutputStream os = t.getResponseBody();
-////                os.write(response.getBytes());
-////                os.close();
-////            } else {
-////                try {
-////                    // create get request to Slack
-////                    HttpClient client = HttpClient.newHttpClient();
-////                    HttpRequest request = HttpRequest.newBuilder()
-////                            .uri(URI.create(createUrl(t.getResponseCode())))
-////                            .build();
-////
-////                    // get response
-////                    HttpResponse<String> response = null;
-////                    response = client.send(request, HttpResponse.BodyHandlers.ofString());
-////                    String res_body = response.body();
-////                    System.out.println(response.body());
-////
-////                    // send response back in exchange
-////                    t.sendResponseHeaders(200, res_body.length());
-////                    OutputStream os = t.getResponseBody();
-////                    os.write(res_body.getBytes());
-////                    os.close();
-////
-////                } catch (InterruptedException e) {
-////                    e.printStackTrace();
-////                }
-////            }
-//        }
-//    }
 }
